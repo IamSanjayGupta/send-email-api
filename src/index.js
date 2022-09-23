c8e966d53c43dcf3d855f04731ccc648107799ed
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-23 19:12:39 +0530

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const nodemailer = require("./NodeMailer/backendmailer");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use("/", nodemailer);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
